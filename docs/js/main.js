b3cc7793e6b42d00725c81b79080f91bf6c79a5e
Aleksandr <s_a_s_h_a_5@mail.ru>
2019-09-17 00:20:43 +0300

    add new section

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,5 +1,28 @@
 $(document).ready(function(){
 	
+	// Вызов слайдера owl-carousel
+	$("#top-slider").owlCarousel({
+		singleItem: true,
+        navigation: true,
+        pagination: true,
+		theme: "top-slider-theme",
+		navigationText : ["",""],
+		slideSpeed: 600
+	});
 	
+	//MixItUp - фильтрация работ в портфолио
+	$('#products-sunglasses').mixItUp();
 	
+    var filterProducts = $('.products-filter__button');
+
+    filterProducts.on('click', function(e) {
+        e.preventDefault();
+        if (filterProducts.hasClass('products-filter__button--active')) {
+            filterProducts.removeClass('products-filter__button--active');
+            $(this).addClass('products-filter__button--active');
+        } else {
+            $(this).addClass('products-filter__button--active');
+        }
+    });
+
 });
